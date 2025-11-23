c0d993cd7f8b75d21895ca624f0acdf6c94464d3
qgdkdzy6fv-cmd <qgdkdzy6fv@privaterelay.appleid.com>
2025-11-23 01:28:10 -0600

    Updated CatalogEditModal.tsx

--- src/components/Catalogs/CatalogEditModal.tsx
+++ src/components/Catalogs/CatalogEditModal.tsx
@@ -76,7 +76,7 @@ export function CatalogEditModal({ isOpen, onClose, catalogName, catalogIcon, ca
               onChange={(e) => setName(e.target.value)}
               onKeyPress={handleKeyPress}
               className="w-full px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent dark:bg-gray-700 dark:text-white"
-              placeholder="Enter catalog name"
+              placeholder={catalogName}
               autoFocus
             />
           </div>
